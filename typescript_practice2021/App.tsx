f725eedffeb75ed8d9f098a1f52f611acd1ff6d7
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-10 16:10:03 +0900

    TypeScript型演習とtsconfig設定

---
+++ typescript_practice2021/App.tsx
@@ -0,0 +1,16 @@
+import './styles.css'
+import { Practice1 } from './Practices/Practice1'
+import { Practice2 } from './Practices/Practice2'
+import { Practice3 } from './Practices/Practice3'
+import { Practice4 } from './Practices/Practice4'
+
+export default function App () {
+    return (
+        <div className="App">
+            <Practice1 />
+            <Practice2 />
+            <Practice3 />
+            <Practice4 />
+        </div>
+    )
+}
